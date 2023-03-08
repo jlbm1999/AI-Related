976469e6b0539acc4ebd28bb282667e48af773b9
Jose Luis Bernaldez Morales <jose-lbm@hotmail.com>
2023-03-08 19:19:39 +0100

    Simple connection test

--- Python ChatGPT/main.py
+++ Python ChatGPT/main.py
@@ -1,1 +1,11 @@
-x = 10
+import openai
+import config
+
+openai.api_key = config.api_key
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role":"user", "content":"¿Cuál es la misión de OpenAI?"}]
+)
+
+print(response)
